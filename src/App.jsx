cd0c0b98e01b6a9f609b4c8d35d98ddf8d8c38f0
AshuJojo <ashutoshsharma.120599@gmail.com>
2024-10-10 11:12:51 +0530

    Initial render homepage implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Home from "./components/Home"
 
 
 function App() {
 
   return (
     <>
-      <p>Hello World</p>
+      <Home />
     </>
   )
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div>
+            <h1>User Details Modal</h1>
+            <button type="button">Open Form</button>
+        </div>
+    )
+}
+
+export default Home
